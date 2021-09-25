d301703fa6a222758f009e92ed1055846bbee6c0
davdleet <davdleet1@gmail.com>
2021-09-25 23:28:29 +0900

    solved 42

---
+++ 42/AA.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int binsearch(int left, int right, int target, vector<int> arr)
+{
+    int middle = (left + right) / 2;
+    int result;
+    if (arr[middle] == target)
+    {
+        return middle;
+    }
+    else
+    {
+        if (target < arr[middle])
+        {
+            return result = binsearch(left, middle - 1, target, arr);
+        }
+        else if (target > arr[middle])
+        {
+            return result = binsearch(middle + 1, right, target, arr);
+        }
+    }
+}
+
+int main()
+{
+    //freopen("input.txt", "rt", stdin);
+    int N, M;
+    cin >> N;
+    cin >> M;
+    vector<int> arr;
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+    sort(arr.begin(), arr.end());
+    int result = binsearch(0, arr.size(), M, arr);
+    cout << result + 1;
+}
