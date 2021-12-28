4021c1a7c36876c895b98cc92733061f2293cdf8
davdleet <davdleet1@gmail.com>
2021-12-28 11:57:01 +0900

    solved 61

---
+++ 61/AA.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[10];
+int treesize;
+int sum = 0;
+int target;
+
+int tree(int level, int num)
+{
+    if (level > treesize)
+    {
+        if (num == target)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        int l_add = tree(level + 1, num + arr[level - 1]);
+        int l_sub = tree(level + 1, num - arr[level - 1]);
+        int r = tree(level + 1, num);
+        return (l_add + l_sub + r);
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "rt", stdin);
+    int N, M;
+    cin >> N >> M;
+    treesize = N;
+    target = M;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    int count = tree(1, 0);
+    cout << count;
+}
