3ed9588a441cdf250a424cb3720ad5ec838e94d1
davdleet <davdleet1@gmail.com>
2022-01-25 20:05:20 +0900

    fixed 16916

--- BOJ/16916/A.py
+++ BOJ/16916/A.py
@@ -13,45 +13,32 @@ current_hash = 0
 target_hash = 0
 MOD = 2 ** 64
 
-x = 27
-target_hash = ord(P[len(P) - 1]) - ord('a') + 1
-for i in range(len(P)-2, -1, -1):
+x = 1
+for i in range(len(P)-1, -1, -1):
     target_hash = target_hash + ((ord(P[i]) - ord('a') + 1) * x)
-    if i != 0:
-        x = x * 27
-        x = x % MOD
-x = 27
-x1 = 1
-p1 = len(S) - 1
-p2 = len(S) - 1 - window_size
+    target_hash = target_hash % MOD
+    x *= 27
+    x %= MOD
 found = False
-current_hash = (ord(S[len(S) - 1]) - ord('a') + 1) * x1
-for i in range(len(S) - 2, len(S) - window_size - 1, -1):
+x = 1
+for i in range(len(P)-1, -1, -1):
     current_hash = current_hash + ((ord(S[i]) - ord('a') + 1) * x)
-    if i != (len(S) - window_size):
-        x = x * 27
-        x = x % MOD
-if window_size == 1:
-    x = 1
+    current_hash = current_hash % MOD
+    if i > 0:
+        x *= 27
+        x %= MOD
 if current_hash == target_hash:
     print(1)
     sys.exit(0)
 
-for i in range(len(S)-window_size-1, -1, -1):
+for i in range(len(S) - len(P)):
+    current_hash -= (ord(S[i]) - ord('a') + 1)*x
+    current_hash = current_hash * 27
+    current_hash = current_hash + (ord(S[i+window_size]) - ord('a') + 1)
+    current_hash %= MOD
     if current_hash == target_hash:
         found = True
         break
-    print(current_hash)
-    current_hash = current_hash - \
-        ((ord(S[i+window_size]) - ord('a') + 1) * x1)
-
-    current_hash = current_hash / 27
-    current_hash = current_hash + ((ord(S[i]) - ord('a') + 1) * x)
-    if current_hash == target_hash:
-        found = True
-        break
-if(current_hash == target_hash):
-    found = True
 if found:
     print(1)
 else:
