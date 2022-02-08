f63171d5d4d276027e81509d161e435f5bd4a21f
davdleet <davdleet1@gmail.com>
2022-02-08 17:39:30 +0900

    solved using sstack

--- BOJ/2812/A.py
+++ BOJ/2812/A.py
@@ -7,25 +7,20 @@ N, K = list(map(int, input().rstrip().split()))
 
 num = list(map(int, input().rstrip()))
 ans = []
+count = 0
 for i in range(len(num)):
-    if len(ans) == N - K:
-        break
-    if len(num) + len(ans) == N - K:
-        ans.append(num[0])
-        num = num[1:]
-    # length of num + ans is more than N - K
+    now = num[i]
+    if len(ans) == 0 or now <= ans[-1]:
+        None
     else:
-        diff = N - K - len(ans) - len(num)
-        max_idx = num.index(max(num[:-diff+1]))
-        ans.append(num[max_idx])
-        num = num[max_idx+1:]
+        while len(ans) != 0 and now > ans[-1] and count < K:
+            ans.pop()
+            count += 1
+    ans.append(now)
+
+while count < K:
+    ans.pop()
+    count += 1
 
 for c in ans:
     print(c, end='')
-
-# base = 10 ** (N-K-1)
-# ans = 0
-# for c in num:
-#     ans = ans + c * base
-#     base = base // 10
-# print(ans)
