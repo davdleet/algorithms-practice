7212e5b70eac9e2d026374d67669859bba4ff39d
davdleet <davdleet1@gmail.com>
2022-03-02 21:38:50 +0900

    solved 2583

--- BOJ/2583/A.py
+++ BOJ/2583/A.py
@@ -2,7 +2,7 @@ import sys
 
 sys.setrecursionlimit(10**6)
 sys.stdin = open(
-    "/Users/sungwonlee/Documents/Projects/algorithms-practice/BOJ/2583/input.txt")
+    "D:\Projects\AA\BOJ\\2583\input.txt")
 input = sys.stdin.readline
 M, N, K = map(int, input().rstrip().split())
 
@@ -17,34 +17,36 @@ for i in range(K):
     for j in range(c1[1], c1[1]+(c2[1] - c1[1])):
         for k in range(c1[0], c1[0]+(c2[0] - c1[0])):
             arr[j][k] = 1
-
 dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
 def DFS(x, y):
     t = False
+    result = 0
+    result += 1
     for i in range(len(dir)):
         a = dir[i][0]
         b = dir[i][1]
         if (x+a < 0 or x+a >= N) or (y+b < 0 or y+b >= M):
-            continue
-        if arr[x+a][y+b] == 0 and check[x+a][y+b] == 0:
-            print(x+a, y+b)
+            None
+        elif arr[y+b][x+a] == 0 and check[y+b][x+a] == 0:
             t = True
-            check[x+a][y+b] = 1
-            return DFS(x+a, y+b)+1
+            check[y+b][x+a] = 1
+            result += DFS(x+a, y+b)
     if not t:
         return 1
+    return result
 
 
 areas = []
+# i is y, j is x
 for i in range(M):
     for j in range(N):
         if arr[i][j] == 0 and check[i][j] == 0:
             check[i][j] = 1
-            area = DFS(i, j)
+            area = DFS(j, i)
             areas.append(area)
-            print(area)
-            print('\n')
 areas.sort()
-print(areas)
+print(len(areas))
+for a in areas:
+    print(a, end=' ')
