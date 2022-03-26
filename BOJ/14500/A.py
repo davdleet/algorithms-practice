89dcf1420dd10a18a28e356aec1c7281e361a1d9
davdleet <davdleet1@gmail.com>
2022-03-26 15:20:27 +0900

    solved 14500

--- BOJ/14500/A.py
+++ BOJ/14500/A.py
@@ -3,11 +3,11 @@ from collections import deque
 sys.stdin = open('D:\Projects\AA\BOJ\\14500\input.txt')
 input = sys.stdin.readline
 
+sys.setrecursionlimit(10**5)
 N, M = map(int, input().rstrip().split())
 arr = []
 
 dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-diagonal = [(1, 1), (-1, -1)]
 
 for i in range(N):
     line = list(map(int, input().rstrip().split()))
@@ -17,41 +17,66 @@ ans = 0
 
 level = 4
 
+ch = {}
 
-def DFS(xi, yi):
-    ch = {}
-    global ans, level
-    cnt = 0
-    st = deque([(xi, yi, cnt, 0)])
-    ch[(xi, yi)] = 1
-    while st:
-        current = st.pop()
-        x = current[0]
-        y = current[1]
-        l = current[2]
-        sum = current[3]
-        sum = sum + arr[x][y]
-        if l == level - 1:
-            if sum > ans:
-                ans = sum
+xi = 0
+yi = 0
+
+
+def DFS(x, y, l, sum):
+    global ch, ans, xi, yi
+    ch[(x, y)] = 1
+    sum = sum + arr[x][y]
+    if l == level - 1:
+        if sum > ans:
+            ans = sum
+        return
+    # handle the T shape
+    elif l == level - 3:
+        direction = ((x - xi), (y - yi))
+        p1 = (direction[1], direction[0])
+        p2 = (-direction[1], -direction[0])
+        v0 = 0
+        v1 = 0
+        v2 = 0
+        if ((x+direction[0] >= N) or (x+direction[0] < 0)) or ((y + direction[1] >= M) or (y + direction[1] < 0)):
+            None
+        else:
+            v0 = (arr[x + direction[0]][y + direction[1]])
+        if ((x + p1[0] >= N) or (x + p1[0] < 0)) or ((y + p1[1] >= M) or (y + p1[1] < 0)):
+            None
+        else:
+            v1 = arr[x + p1[0]][y + p1[1]]
+        if ((x + p2[0] >= N) or (x + p2[0] < 0)) or ((y + p2[1] >= M) or (y + p2[1] < 0)):
+            None
+        else:
+            v2 = arr[x + p2[0]][y + p2[1]]
+        if sum + v0 + v1 > ans:
+            ans = sum + v0 + v1
+        if sum + v0 + v2 > ans:
+            ans = sum + v0 + v1
+    for i in range(len(dir)):
+        a = dir[i][0]
+        b = dir[i][1]
+        if (x+a < 0 or x+a >= N) or (y+b < 0 or y+b >= M):
             continue
-        # handle the T shape
-        for i in range(4):
-            a = dir[i][0]
-            b = dir[i][1]
-            if (x+a < 0 or x+a >= N) or (y+b < 0 or y+b >= M):
+        try:
+            t = ch[((x+a), (y+b))]
+            if t == 1:
                 continue
-            try:
-                t = ch[((x+a), (y+b))]
-                continue
-            except:
-                ch[((x+a), (y+b))] = 1
-            st.append((x+a, y+b, l+1, sum))
+        except:
+            # ch[((x+a), (y+b))] = 1
+            None
+        DFS(x+a, y+b, l+1, sum)
+        ch[((x+a), (y+b))] = 0
+    ch[(x, y)] = 0
 
 
 for i in range(N):
     for j in range(M):
-        DFS(i, j)
+        xi = i
+        yi = j
+        DFS(i, j, 0, 0)
 
 
 print(ans)
