18fd77dc6d71768a0b3eacca65c9af184d6fc292
davdleet <davdleet1@gmail.com>
2022-04-01 23:45:54 +0900

    fixed infinite loop

--- BOJ/9019/A.py
+++ BOJ/9019/A.py
@@ -5,14 +5,16 @@ sys.stdin = open("D:\Projects\AA\BOJ\9019\input.txt")
 input = sys.stdin.readline
 
 T = int(input().rstrip())
-# ch = [100000] * 10000
+ch = [100000] * 10000
 
 for i in range(T):
     start, end = map(int, input().rstrip().split())
     ans = ''
     cnt = 10**6
     st = deque([(start, '', 0)])
+    times = 0
     while st:
+        times += 1
         current = st.popleft()
         num = current[0]
         ins = current[1]
@@ -23,16 +25,18 @@ for i in range(T):
             break
         a1 = (num * 2) % 10000
         a2 = ins + 'D'
-        # if len(a2) < ch[a1]:
-        #     st.append((a1, a2, 0))
-        st.append((a1, a2, 0))
-        b1 = num - 1
-        if num <= 0:
-            num = 9999 + num
+        if len(a2) < ch[a1] and a2 != current:
+            st.append((a1, a2, 0))
+            ch[a1] = len(a2)
+        b1 = 0
+        if num == 0:
+            b1 = 9999
+        else:
+            b1 = num - 1
         b2 = ins + 'S'
-        # if len(b2) < ch[b1]:
-        #     st.append((b1, b2, 0))
-        st.append((b1, b2, 0))
+        if len(b2) < ch[b1]:
+            st.append((b1, b2, 0))
+            ch[b1] = len(b2)
         new_str = str(num)
         if len(str(num)) < 4:
             to_add = '0' * (4 - len(str(num)))
@@ -40,16 +44,14 @@ for i in range(T):
         if sh <= 2:
             c1 = new_str[1:] + new_str[0]
             c2 = ins + 'L'
-            # if len(c2) < ch[int(c1)]:
-            #     st.append((int(c1), c2, sh+1))
-            st.append((int(c1), c2, sh+1))
+            if len(c2) < ch[int(c1)]:
+                st.append((int(c1), c2, sh+1))
+                ch[int(c1)] = len(c2)
             if sh < 2:
                 d1 = new_str[3] + new_str[0:3]
                 d2 = ins + 'R'
-                # if len(d2) < ch[int(d1)]:
-                #     st.append((int(d1), d2, sh+1))
-                st.append((int(d1), d2, sh+1))
+                if len(d2) < ch[int(d1)]:
+                    st.append((int(d1), d2, sh+1))
+                    ch[int(d1)] = len(d2)
         cnt += 1
     print(ans)
-
-# for i in range(T):
