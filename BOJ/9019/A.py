eb781bd21d5cd5aefcf0ad97d2fc04ccac37cea6
davdleet <davdleet1@gmail.com>
2022-04-02 01:25:11 +0900

    solved 9019

--- BOJ/9019/A.py
+++ BOJ/9019/A.py
@@ -1,32 +1,28 @@
 import sys
 from collections import deque
 
-sys.stdin = open("D:\Projects\AA\BOJ\9019\input.txt")
+sys.stdin = open('D:\Projects\AA\BOJ\9019\input.txt')
 input = sys.stdin.readline
-
 T = int(input().rstrip())
-ch = [100000] * 10000
 
 for i in range(T):
+    ch = [10**6] * 10000
     start, end = map(int, input().rstrip().split())
     ans = ''
-    cnt = 10**6
-    st = deque([(start, '', 0)])
+    q = deque([(start, '')])
     times = 0
-    while st:
+    while q:
         times += 1
-        current = st.popleft()
+        current = q.popleft()
         num = current[0]
         ins = current[1]
-        sh = current[2]
         if num == end:
-            if len(ins) < cnt:
-                ans = ins
+            ans = ins
             break
         a1 = (num * 2) % 10000
         a2 = ins + 'D'
-        if len(a2) < ch[a1] and a2 != current:
-            st.append((a1, a2, 0))
+        if len(a2) < ch[a1]:
+            q.append((a1, a2))
             ch[a1] = len(a2)
         b1 = 0
         if num == 0:
@@ -35,23 +31,18 @@ for i in range(T):
             b1 = num - 1
         b2 = ins + 'S'
         if len(b2) < ch[b1]:
-            st.append((b1, b2, 0))
+            q.append((b1, b2))
             ch[b1] = len(b2)
-        new_str = str(num)
-        if len(str(num)) < 4:
-            to_add = '0' * (4 - len(str(num)))
-            new_str = to_add + str(num)
-        if sh <= 2:
-            c1 = new_str[1:] + new_str[0]
-            c2 = ins + 'L'
-            if len(c2) < ch[int(c1)]:
-                st.append((int(c1), c2, sh+1))
-                ch[int(c1)] = len(c2)
-            if sh < 2:
-                d1 = new_str[3] + new_str[0:3]
-                d2 = ins + 'R'
-                if len(d2) < ch[int(d1)]:
-                    st.append((int(d1), d2, sh+1))
-                    ch[int(d1)] = len(d2)
-        cnt += 1
+        zeros = '0' * (4 - len(str(num)))
+        new_str = zeros + str(num)
+        c1 = new_str[1:] + new_str[0]
+        c2 = ins + 'L'
+        if len(c2) < ch[int(c1)]:
+            q.append((int(c1), c2))
+            ch[int(c1)] = len(c2)
+        d1 = new_str[3] + new_str[0:3]
+        d2 = ins + 'R'
+        if len(d2) < ch[int(d1)]:
+            q.append((int(d1), d2))
+            ch[int(d1)] = len(d2)
     print(ans)
