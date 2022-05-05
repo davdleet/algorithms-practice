3a15f1f09cc2c6858120b6422170075a438b4c68
davdleet <davdleet1@gmail.com>
2022-05-05 10:53:57 +0900

    minor edit

--- BOJ/9375/A.py
+++ BOJ/9375/A.py
@@ -20,10 +20,4 @@ for i in range(T):
     for key in wear.keys():
         l = len(wear[key])
         cnt = cnt + l + cnt * l
-    # for key in wear.keys():
-    #     t = list(d.keys())
-    #     for ex in t:
-    #         d[ex+key] = 1
-    #     for item in wear[key]:
-    #         d[item] = 1
     print(cnt)
