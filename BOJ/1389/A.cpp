f5518cdbbb32f6f3cc3ce8c17bedbba30bcaa7cc
davdleet <davdleet1@gmail.com>
2022-05-10 22:45:41 +0900

    solved 1389

--- BOJ/1389/A.cpp
+++ BOJ/1389/A.cpp
@@ -10,10 +10,9 @@ int main()
     int N, M;
     cin >> N >> M;
     vector<vector<int>> arr;
-    vector<int> sums(N + 1, 0);
     for (int i = 0; i < N + 1; i++)
     {
-        vector<int> temp(N + 1, 0);
+        vector<int> temp(N + 1, 1000000);
         arr.push_back(temp);
     }
     for (int i = 0; i < M; i++)
@@ -22,12 +21,18 @@ int main()
         cin >> a >> b;
         arr[a][b] = 1;
         arr[b][a] = 1;
-        sums[a] += 1;
-        sums[b] += 1;
     }
-    bool action = false;
-    int loop_cnt = 0;
-    while (!action)
+    // for (int i = 1; i < arr.size(); i++)
+    // {
+    //     for (int j = 1; j < arr[i].size(); j++)
+    //     {
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    bool action = true;
+    while (action)
     {
         action = false;
         for (int i = 1; i < N + 1; i++)
@@ -38,80 +43,45 @@ int main()
                 {
                     continue;
                 }
-                else
+                for (int k = 1; k < N + 1; k++)
                 {
-                    if (arr[i][j])
+                    if (k == i)
                     {
-                        for (int k = 1; k < N + 1; k++)
-                        {
-                            if (k == i)
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                if (arr[j][k])
-                                {
-                                    action = true;
-                                    if (arr[i][k])
-                                    {
-                                        sums[i] -= arr[i][k];
-                                        int temp = min(arr[i][k], arr[j][k] + 1);
-                                        arr[i][k] = temp;
-                                        sums[i] += temp;
-                                    }
-                                    else
-                                    {
-                                        arr[i][k] = arr[j][k] + 1;
-                                        sums[i] += arr[j][k] + 1;
-                                    }
-                                }
-                                int temp2 = min(arr[k][i], arr[j][k] + 1);
-                                if (arr[k][i])
-                                {
-                                    action = true;
-                                    sums[i] -= arr[k][i];
-
-                                    arr[k][i] = temp2;
-                                    sums[i] += temp2;
-                                }
-                                else
-                                {
-                                    action = true;
-                                    arr[k][i] = temp2;
-                                    sums[i] += temp2;
-                                }
-                            }
-                        }
+                        continue;
+                    }
+                    if (arr[i][j] + arr[j][k] < arr[i][k])
+                    {
+                        action = true;
+                        arr[i][k] = arr[i][j] + arr[j][k];
                     }
                 }
             }
         }
     }
-    int num = 90000000;
-    int smallest_idx = -1;
+
+    int min_idx = 0;
+    int min_num = 100000000;
     for (int i = 1; i < N + 1; i++)
     {
-        cout << sums[i] << "\n";
-        if (sums[i] < num)
+        int temp = 0;
+        for (int j = 1; j < N + 1; j++)
         {
-            num = sums[i];
-            smallest_idx = i;
+            temp += arr[i][j];
+        }
+        if (temp < min_num)
+        {
+            min_num = temp;
+            min_idx = i;
         }
     }
-    // for (int i = 1; i < N + 1; i++)
-    // {
-    //     cout << sums[i] << " ";
-    // }
-
     // cout << endl;
-    // for (int i = 1; i < N + 1; i++)
+    // for (int i = 1; i < arr.size(); i++)
     // {
-    //     for (int j = 1; j < N + 1; j++)
+    //     for (int j = 1; j < arr[i].size(); j++)
     //     {
     //         cout << arr[i][j] << " ";
     //     }
     //     cout << "\n";
     // }
-    cout << smallest_idx;
+    cout << min_idx;
 }
