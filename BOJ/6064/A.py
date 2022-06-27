80e669e22d966ea812329c4d4aeab9dd46dc8707
davdleet <davdleet1@gmail.com>
2022-06-27 21:52:17 +0900

    solved 6064

--- BOJ/6064/A.py
+++ BOJ/6064/A.py
@@ -5,13 +5,41 @@ input = sys.stdin.readline
 
 
 T = int(input().rstrip())
-print(T)
+
+
+def LCM(x, y):
+    num = x
+    while True:
+        if num % y == 0:
+            return num
+        num = num + x
+
 
 for i in range(T):
     M, N, X, Y = map(int, input().rstrip().split())
-    found = False
-    num = 0
-    x = 0
-    y = 0
-    while not found:
-        x = num +
+    last = LCM(M, N)
+    if M == X and N == Y:
+        print(last)
+        continue
+    x = 1
+    y = 1
+    year = 1
+    ans = 0
+    x = X
+    year = year + X-1
+    y = y+X-1
+    y = y % N
+    if y == 0:
+        y = N
+    while True:
+        if year < last and y == Y:
+            ans = year
+            break
+        elif year >= last:
+            ans = -1
+            break
+        year = year + M
+        y = (y + M) % N
+        if y == 0:
+            y = N
+    print(ans)
