70fe05c882f63235b4f2cf9ee72c6f5ac93f5d2a
davdleet <davdleet1@gmail.com>
2022-07-08 22:53:19 +0900

    solving 2750

---
+++ BOJ/2750/A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MAX_NUM 1000
+
+vector<int> arr;
+
+void mergesort(int l, int r)
+{
+    int m = (l + r) / 2;
+}
+
+void merge()
+{
+}
+
+int main()
+{
+    freopen("input.txt", "rt", stdin);
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+}
